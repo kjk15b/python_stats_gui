cb63f721c0c5045e5e536af12d6faa7ab8bccd81
Kolby Kiesling <kolbykiesling@Kolbys-Macbook-Air.local>
2019-09-03 08:19:20 -0500

    Updated functions, (x,y) formatting

--- basic_gui_stats.py
+++ basic_gui_stats.py
@@ -16,10 +16,12 @@ import tkinter as tk
 from tkinter import ttk
 import matplotlib.pyplot as plt
 import numpy as np
+import re
 
 
 data_array = list()
 
+xy_array = [[], []]
 
 data_message = ""
 
@@ -133,6 +135,70 @@ def add_data(*args):
     except ValueError:
         pass
         
+def clear_xy():
+    global xy_array
+    xy_array = [[], []]
+    print(xy_array)
+    xstd.set(0)
+    xavg.set(0)
+    xmx.set(0)
+    xmn.set(0)
+    xvariance.set(0)
+    xco_var.set(0)
+    xmed.set(0)
+    xmn.set(0)
+    xpoints.set(0)
+    ystd.set(0)
+    yavg.set(0)
+    ymx.set(0)
+    ymn.set(0)
+    yvariance.set(0)
+    yco_var.set(0)
+    ymed.set(0)
+    ymn.set(0)
+    ypoints.set(0)
+    
+def stats_xy():
+    xstd.set(np.std(xy_array[0]))
+    xavg.set(np.average(xy_array[0]))
+    xmx.set(np.amax(xy_array[0]))
+    xmn.set(np.amin(xy_array[0]))
+    xvariance.set(np.var(xy_array[0]))
+    xco_var.set(np.std(xy_array[0]) / np.average(xy_array[0]) * 100)
+    xmed.set(np.median(xy_array[0]))
+    xmn.set(np.amin(xy_array[0]))
+    xpoints.set(len(xy_array[0]))
+    
+    ystd.set(np.std(xy_array[1]))
+    yavg.set(np.average(xy_array[1]))
+    ymx.set(np.amax(xy_array[1]))
+    ymn.set(np.amin(xy_array[1]))
+    yvariance.set(np.var(xy_array[1]))
+    yco_var.set(np.std(xy_array[1]) / np.average(xy_array[1]) * 100)
+    ymed.set(np.median(xy_array[1]))
+    ymn.set(np.amin(xy_array[1]))
+    ypoints.set(len(xy_array[1]))
+
+def add_xy(*args):
+    try:
+        g = int(xy_mute.get())
+        ret = str(xydata.get())
+        ret = re.findall(r"[-+]?\d*\.\d+|\d+", ret)
+        ret = list(map(float, ret))
+        xy_array[0].append(ret[0])
+        xy_array[1].append(ret[1])
+        print("X:\t", str(ret[0]), "\tY:\t", str(ret[1]), "\n")
+        stats_xy()
+        if g > 0:
+            plot_xy()
+    except ValueError:
+        pass
+
+def plot_xy(*args):
+    xind, yind = len(xy_array[0]) - 1, len(xy_array[1]) - 1
+    plt.scatter(xy_array[0][xind], xy_array[1][yind], color='red', marker='^')
+    plt.pause(0.05)
+
 
 root = Tk()
 root.title("Simple Laboratory Statistics Calculator")
@@ -168,6 +234,8 @@ data_mute = IntVar()
 
 scrollbar = Scrollbar(root)
 
+xydata = StringVar()
+
 
 scrollbar.grid(column=8, row=0, sticky='ns')
 dlist = Listbox(root, yscrollcommand=scrollbar.set)
@@ -176,6 +244,86 @@ scrollbar.config(command = dlist.yview)
 
 data_entry = ttk.Entry(mainframe, width=4, textvariable=data)
 data_entry.grid(column=0, row=0, stick=(W, E))
+
+data_xy = ttk.Entry(mainframe, width=4, textvariable=xydata)
+data_xy.grid(column=0, row=9, stick=(W, E))
+ttk.Label(mainframe, text="X & Y Data").grid(column=1, row=9, sticky=(W, E))
+ttk.Label(mainframe, text="Format: 'X, Y'").grid(column=2, row=9, sticky=(W, E))
+
+
+ttk.Button(mainframe, text="Clear", command=clear_xy).grid(column=3, row=9, sticky=W)
+# For XY Data Format
+
+xstd = StringVar()
+xavg = StringVar()
+xmx = StringVar()
+xmn = StringVar()
+xvariance = StringVar()
+xco_var = StringVar()
+xmed = StringVar()
+xpoints = StringVar()
+
+ystd = StringVar()
+yavg = StringVar()
+ymx = StringVar()
+ymn = StringVar()
+yvariance = StringVar()
+yco_var = StringVar()
+ymed = StringVar()
+ypoints = StringVar()
+
+xy_mute = IntVar()
+
+Checkbutton(mainframe, text="XY Graph", variable=xy_mute).grid(column=4, row=9, sticky=W)
+
+ttk.Label(mainframe, textvariable=xstd).grid(column=0, row=10, sticky=(W, E))
+ttk.Label(mainframe, text="[X] Std-Dev").grid(column=1, row=10, sticky=(W, E))
+
+ttk.Label(mainframe, textvariable=xavg).grid(column=0, row=11, sticky=(W, E))
+ttk.Label(mainframe, text="[X] Average").grid(column=1, row=11, sticky=(W, E))
+
+ttk.Label(mainframe, textvariable=xmx).grid(column=0, row=12, sticky=(W, E))
+ttk.Label(mainframe, text="[X] Maximum").grid(column=1, row=12, sticky=(W, E))
+
+ttk.Label(mainframe, textvariable=xmn).grid(column=0, row=13, sticky=(W, E))
+ttk.Label(mainframe, text="[X] Minimum").grid(column=1, row=13, sticky=(W, E))
+
+ttk.Label(mainframe, textvariable=xvariance).grid(column=0, row=14, sticky=(W, E))
+ttk.Label(mainframe, text="[X] Variance").grid(column=1, row=14, sticky=(W, E))
+
+ttk.Label(mainframe, textvariable=xco_var).grid(column=0, row=15, sticky=(W, E))
+ttk.Label(mainframe, text="[X] Co-Variance [%]").grid(column=1, row=15, sticky=(W, E))
+
+ttk.Label(mainframe, textvariable=xmed).grid(column=0, row=16, sticky=(W, E))
+ttk.Label(mainframe, text="[X] Median").grid(column=1, row=16, sticky=(W, E))
+
+ttk.Label(mainframe, textvariable=xpoints).grid(column=0, row=17, sticky=(W, E))
+ttk.Label(mainframe, text="[X] # of Points").grid(column=1, row=17, sticky=(W, E))
+
+ttk.Label(mainframe, textvariable=ystd).grid(column=2, row=10, sticky=(W, E))
+ttk.Label(mainframe, text="[Y] Std-Dev").grid(column=3, row=10, sticky=(W, E))
+
+ttk.Label(mainframe, textvariable=yavg).grid(column=2, row=11, sticky=(W, E))
+ttk.Label(mainframe, text="[Y] Average").grid(column=3, row=11, sticky=(W, E))
+
+ttk.Label(mainframe, textvariable=ymx).grid(column=2, row=12, sticky=(W, E))
+ttk.Label(mainframe, text="[Y] Maximum").grid(column=3, row=12, sticky=(W, E))
+
+ttk.Label(mainframe, textvariable=ymn).grid(column=2, row=13, sticky=(W, E))
+ttk.Label(mainframe, text="[Y] Minimum").grid(column=3, row=13, sticky=(W, E))
+
+ttk.Label(mainframe, textvariable=yvariance).grid(column=2, row=14, sticky=(W, E))
+ttk.Label(mainframe, text="[Y] Variance").grid(column=3, row=14, sticky=(W, E))
+
+ttk.Label(mainframe, textvariable=yco_var).grid(column=2, row=15, sticky=(W, E))
+ttk.Label(mainframe, text="[Y] Co-Variance [%]").grid(column=3, row=15, sticky=(W, E))
+
+ttk.Label(mainframe, textvariable=ymed).grid(column=2, row=16, sticky=(W, E))
+ttk.Label(mainframe, text="[Y] Median").grid(column=3, row=16, sticky=(W, E))
+
+ttk.Label(mainframe, textvariable=ypoints).grid(column=2, row=17, sticky=(W, E))
+ttk.Label(mainframe, text="[Y] # of Points").grid(column=3, row=17, sticky=(W, E))
+
 
 plt_title = ttk.Entry(mainframe, width=4, textvariable=title)
 plt_title.grid(column=3, row=0, stick=(W, E))
@@ -244,8 +392,11 @@ for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
 
 data_entry.focus()
 root.bind('<Return>', add_data)
+root.bind('<Tab>', add_xy)
 
 fig, ax = plt.subplots()
+
+plt.figure(10)
 
 root.mainloop()
 
